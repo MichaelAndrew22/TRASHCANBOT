fd6711ef17a1efe8eb607c068cc2114347ccc8d7
MichaelAndrew22 <143136160+MichaelAndrew22@users.noreply.github.com>
2023-11-06 23:17:06 -0800

    socket implementation

--- app.py
+++ app.py
@@ -2,12 +2,22 @@ from flask import Flask, render_template, request
 from motors import Motors
 import pyfirmata
 import time
+from flask_socketio import SocketIO, emit
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '1234'
+socketio = SocketIO(app)
+socketio.init_app(app, cors_allowed_origins="*")
+
+motors = Motors("/dev/cu.usbmodem2201")
+
 
 
 @app.route('/', methods=['POST','GET'])
 def index():
     return render_template('index.html')
+
+"""
 
 
 @app.route('/forward', methods=['GET'])
@@ -48,8 +58,62 @@ def stop():
     motors.stop()
     print(motors)
     return render_template('index.html',motors=motors)
+"""
+
+@socketio.on('connect')
+def test_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+    socketio.emit('my response', json)
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+    socketio.emit('message', message)
+
+    if message == 'forward':
+
+        motors.set_speed(0.5)
+        motors.forward()
+        print(motors)
+    elif message == 'backward':
+
+        motors.set_speed(0.5)
+        motors.backward()
+        print(motors)
+    elif message == 'left':
+
+        motors.set_speed(0.5)
+        motors.turn_left()
+        print(motors)
+    elif message == 'right':
+
+        motors.set_speed(0.5)
+        motors.turn_right()
+        print(motors)
+    elif message == 'stop':
+
+        motors.stop()
+        print(motors)
+    else:
+        print('no message')
+
+
+
+
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True)
 
+"""
+if __name__ == '__main__':
+    app.run()
+"""
 
