168198b9441bfe38c704ceadf111f5fa608b736e
MichaelAndrew22 <143136160+MichaelAndrew22@users.noreply.github.com>
2023-11-07 22:12:49 -0800

    opencv cam feed

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 from motors import Motors
 from flask_socketio import SocketIO
+import cv2
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1234'
@@ -64,10 +66,35 @@ def handle_message(message):
     socketio.emit('message', message)
 
 
+#OPENCV PORTION
+
+camera = cv2.VideoCapture(0)
+
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()  # read the camera frame
+
+        if not success:
+            break
+        else:
+            frame = cv2.resize(frame, (480, 320))
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app,debug=True)
+
+
+
+
 
 
 
