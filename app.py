5478ba52d580f22962c654bc403c596d0b9b020b
MichaelAndrew22 <143136160+MichaelAndrew22@users.noreply.github.com>
2023-11-29 23:42:24 -0800

    movement display

--- app.py
+++ app.py
@@ -36,43 +36,33 @@ def handle_my_custom_event(json):
     message = json['data']
     message = message.lower()
 
-    speed = json['speed']
-    #motors.set_speed(speed)
-
     if message == 'forward':
         motors.forward()
         sleep(2)
         motors.stop()
-       # print(motors)
 
     elif message == 'backward':
         motors.backward()
         sleep(2)
         motors.stop()
-        #print(motors)
 
     elif message == 'left':
         motors.turn_left()
         sleep(2)
         motors.stop()
-        #print(motors)
 
     elif message == 'right':
         motors.turn_right()
         sleep(2)
         motors.stop()
-        #print(motors)
 
     elif message == 'stop':
         motors.stop()
-        #print(motors)
-
 
 #OPENCV PORTION
 
 camera = cv2.VideoCapture(0)
 camera.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
-
 
 def gen_frames():
     while True:
@@ -85,8 +75,6 @@ def gen_frames():
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
 
 
 if __name__ == '__main__':
