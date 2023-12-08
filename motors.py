93bcfa3fb6f3785f2a9bf8395f5cd2267f52afa7
MichaelAndrew22 <143136160+MichaelAndrew22@users.noreply.github.com>
2023-12-07 21:58:29 -0800

    turn_adjustments

--- motors.py
+++ motors.py
@@ -15,12 +15,14 @@ class Motors:
         self.rightMotor.forward(0.4)
 
     def turn_left(self):
-        self.leftMotor.backward(0.0)
-        self.rightMotor.backward(0.4)
+        #self.leftMotor.backward(0.0)
+        self.leftMotor.forward(0.2)
+        self.rightMotor.backward(0.7)
 
     def turn_right(self):
-        self.leftMotor.forward(0.4)
-        self.rightMotor.forward(0.0)
+        self.leftMotor.forward(0.7)
+        #self.rightMotor.forward(0.0)
+        self.rightMotor.backward(0.2)
 
     def stop(self):
         self.leftMotor.stop()
